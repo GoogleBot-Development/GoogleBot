5ac794e406ee92b4791a78e69bd020d317d11c92
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-07-28 18:52:15 -0500

    Create servercount.js

---
+++ commands/Utility/servercount.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
+
+exports.run = (client, message, args) => {
+client.shard.fetchClientValues('guilds.cache.size')
+    .then(result => {
+      message.channel.send(`Total Shards: 2\nTotal Servers per Shard:\n${result}`)
+    })
+    client.shard.fetchClientValues('guilds.cache.size')
+	.then(results => {
+		message.channel.send(`${results.reduce((prev, guildCount) => prev + guildCount, 0)} total servers!`);
+	})
+	.catch(console.error);
+}
+
+exports.help = {
+  name: "servercount",
+  description: "I will show my servercount here, as well as how many servers are on each shard.",
+  usage: "servercount",
+  category: "Utility"
+}
+
+exports.aliases = ["sc", "servers"]
