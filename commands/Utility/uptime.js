abc70a831826e8516eab648b412e50aac0ae4ddb
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-07-28 18:52:49 -0500

    Create uptime.js

---
+++ commands/Utility/uptime.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
+
+
+exports.run = (client, message, args) => {
+  let days = Math.floor(client.uptime / 86400000);
+    let hours = Math.floor(client.uptime / 3600000) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    let seconds = Math.floor(client.uptime / 1000) % 60;
+    let uptime = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+      let statsEmbed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setAuthor("GoogleBot Uptime", message.author.displayAvatarURL({format: "png"}))
+      .addField("⏰ Uptime",uptime)
+      .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( {format: "png"} ))
+      .setTimestamp()
+      message.channel.send(statsEmbed)
+}
+
+exports.help = {
+  name: "uptime",
+  description: "Displays my uptime!",
+  usage: "uptime",
+  category: "Utility"
+}
+
+exports.aliases = []
