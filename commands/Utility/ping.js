e3083b01678bae65ff1cac1379a5f92efb891eea
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-07-30 14:47:46 -0500

    Update ping.js

--- commands/Utility/ping.js
+++ commands/Utility/ping.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js')
 const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
 
-exports.run = (client, message, args, async) => {
-      var ping = Date.now() - message.createdTimestamp + "ms";
+exports.run = (client, message, args) => {
+  message.channel.send("Getting the ping...").then(msg => {
+      msg.delete()
+      var ping = msg.createdTimestamp - message.createdTimestamp + "ms";
 let pingEmbed = new Discord.MessageEmbed()
      .setColor("RANDOM")
      .setTitle("Pong!")
      .setThumbnail('https://cdn.discordapp.com/attachments/719702469654872154/727779104660652052/image0.png')
-     .addField("Bot Latency:", `:hourglass_flowing_sand: ${client.ws.ping}ms`)
-     .addField("Message Ping:", `:hourglass_flowing_sand: ${ping}`)
+     .addField("Bot Latency:", `:hourglass_flowing_sand: ${client.ws.ping}ms`, true)
+     .addField("Message Ping:", `:hourglass_flowing_sand: ${ping}`, true)
      .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
      .setTimestamp()
      message.channel.send(pingEmbed)
+});
 }
 
 exports.help = {
