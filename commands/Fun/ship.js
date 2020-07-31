072b8565da1a7225964a2cc6786e9db696fe03a9
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-07-31 15:42:48 -0500

    Create ship.js

---
+++ commands/Fun/ship.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
+
+exports.run = (client, message, args, funcs) => {
+  try {
+    let first = args[0];
+    if (!first) return funcs.reply('you must enter the first object to ship!')
+    let second = args[1];
+    if (!second) return funcs.reply('you must enter the second object to ship!')
+    let percentage = Math.floor(Math.random() * 100);
+    let shipEmbed = new Discord.MessageEmbed()
+      shipEmbed.setTimestamp()
+      shipEmbed.setTitle(`Ship`)
+      shipEmbed.setDescription(`I ship \`${first}\` and \`${second}\`, \`${percentage}\`%!`)
+      shipEmbed.setColor("RANDOM");
+      shipEmbed.setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
+    message.channel.send(shipEmbed);
+  } catch (e) {
+    console.error;
+    message.channel.send(`Oh no, an error occurred!\n${e.message}`);
+  }
+};
+
+exports.help = {
+    name: "ship",
+    description: "Ships two things together!",
+    usage: "ship",
+    category: "Fun"
+  }
+  
+  exports.aliases = ["combine"]
