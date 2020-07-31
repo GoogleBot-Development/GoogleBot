d5aa5151b54699cd10132331f1765022006e1461
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-07-31 17:50:39 -0500

    Create emojify.js

---
+++ commands/Fun/emojify.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+const mapping = {
+    ' ': '    ', '0': ' :zero:', '1': ' :one:', '2': ' :two:', '3': ' :three:', '4': ' :four:', '5': ' :five:', '6': ' :six:', '7': ' :seven:', '8': ' :eight:', '9': ' :nine:', '!': ':grey_exclamation:', '?': ':grey_question:', '#': ' :hash:', '*': ' :asterisk:'
+};
+
+'abcdefghijklmnopqrstuvwxyz'.split('').forEach(e => {
+    mapping[e] = mapping[e.toUpperCase()] = ` :regional_indicator_${e}:`;
+});
+    if (args.length < 1) return message.reply('you must send something to emojify!')
+    message.channel.send(
+        args.join(' ')
+            .split('')
+            .map(e => mapping[e] || e)
+            .join('')
+    );
+}
+
+exports.help = {
+    name: "emojify",
+    description: "Emojifies whatever you input!",
+    usage: "emojify [text]",
+    category: "Fun"
+  }
+  
+exports.aliases = ["emoji"]
