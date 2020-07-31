afdb0a3f8f72f73d89d2e10aacf7f0060610bc9a
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-07-31 15:41:55 -0500

    Create advice.js

---
+++ commands/Fun/advice.js
@@ -0,0 +1,30 @@
+const req = require('node-superfetch');
+const Discord = require('discord.js')
+const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
+
+module.exports.run = async (client, message, args) => {
+  try {
+    const {
+      body
+    } = await req.get('http://api.adviceslip.com/advice');
+    let adviceEmbed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setDescription(JSON.parse(body.toString()).slip.advice)
+    .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
+    .setTimestamp()
+    
+    message.channel.send(adviceEmbed);
+  } catch (e) {
+    console.log(e);
+    message.channel.send(`Oh no, an error occurred!\n${e.message}`);
+  }
+};
+
+exports.help = {
+    name: "advice",
+    description: "Gives some advice. Great if you feel bored!",
+    usage: "advice",
+    category: "Fun"
+  }
+  
+  exports.aliases = [""]
