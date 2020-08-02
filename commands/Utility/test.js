298cbd8f6a37c9cebdcd297d67e8f7438e258623
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-01 22:39:47 -0500

    Create test.js

---
+++ commands/Utility/test.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+// For each shard, get the shard ID and the number of guilds it owns
+let values = await client.shard.broadcastEval(`
+    [
+        this.shard.id,
+        this.guilds.size
+    ]
+`);
+// Make a final string which will be sent in the channel
+let finalString = "**SHARD STATUS**\n\n";
+// For each shard data
+values.forEach((value) => {
+    // Add the shard infos to the final string
+    finalString += "• SHARD #"+value[0]+" | ServerCount: "+value[1]+"\n";
+});
+// Send the final string in the channel
+message.channel.send(finalString);
+}
+
+exports.help = {
+    name: "test",
+    description: "Test",
+    usage: "test",
+    category: "Utility"
+  }
+  
+  exports.aliases = []
