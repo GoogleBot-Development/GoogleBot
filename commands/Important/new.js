5041f771e0c81aca0f884c153c418efaf4386ca5
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-01 23:08:26 -0500

    Update new.js

--- commands/Important/new.js
+++ commands/Important/new.js
@@ -12,6 +12,7 @@ exports.run = (client, message, args) => {
     .addField("Version 2.0.5 Update:", "Added a new section: `Action Commands`\nAdded `g!kill <@member>` | Kills the mentions member\nAdded `g!waste <@member>` | Wastes the mentioned member\nAdded `g!punch <@member>` | Punches the mentions member\nAdded `g!like <@member>` | Likes the mentioned member\nAdded `g!lick <@member>` | Licks the mentioned member\nAdded `g!bite <@member>` | Bites the mentioned member\nAdded `g!stats` | Shows GoogleBot's stats")
     .addField("Version 2.1 Update:", "Improved the `userinfo` command! You can now ping the selected user to get their userinfo, or just not ping anyone and get your *own* info!")
     .addField("Version 2.5 Update:", "Added `g!math <equation>` | I can do some simple math equations\nAdded `g!rps <rock/paper/scissors>` | You can play Rock Paper Scissors with me!\nAdded `g!greentext <text>` | I can make your text green!/nAdded `g!ship <object 1> <object 2>` | I can ship two things together with a percent\nAdded `g!advice` | I can give you some advice, if you ever need some.")
+    .addField("Version 2.6 Update:", "Added `g!morse <text>` | I can now translate your text into Morse Code! Only supports letters and numbers in the English language")
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
     .setTimestamp()
     message.channel.send(newEmbed)
