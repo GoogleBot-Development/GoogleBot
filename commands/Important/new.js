6c5a1918904dbe48585df0942a19931667ca0a26
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-02 12:55:46 -0500

    Update new.js

--- commands/Important/new.js
+++ commands/Important/new.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js')
+ const Discord = require('discord.js')
 const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
 
 exports.run = (client, message, args) => {
@@ -13,6 +13,7 @@ exports.run = (client, message, args) => {
     .addField("Version 2.1 Update:", "Improved the `userinfo` command! You can now ping the selected user to get their userinfo, or just not ping anyone and get your *own* info!")
     .addField("Version 2.5 Update:", "Added `g!math <equation>` | I can do some simple math equations\nAdded `g!rps <rock/paper/scissors>` | You can play Rock Paper Scissors with me!\nAdded `g!greentext <text>` | I can make your text green!/nAdded `g!ship <object 1> <object 2>` | I can ship two things together with a percent\nAdded `g!advice` | I can give you some advice, if you ever need some.")
     .addField("Version 2.6 Update:", "Added `g!morse <text>` | I can now translate your text into Morse Code! Only supports letters and numbers in the English language")
+    .addField("Version 2.6.5 Update:", "Updated the `g!morse` command so that you can now input your Morse Code into it, and it will translate back to English! Also added more characters on the keyboard to work with.")
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
     .setTimestamp()
     message.channel.send(newEmbed)
