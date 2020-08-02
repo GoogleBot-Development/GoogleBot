57d6bf05644387acd6756237353b3cca2f08eedd
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-01 22:53:51 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,9 @@
 module.exports = (client) => {
   console.log(`The shard launched successfully!`);
   
-  setInterval(function() {
-      let statuses = [ `g!help | 2 shards`, `g!help | Over 2400 servers!`, `Discord verify bots...` ];
-      let status = statuses[Math.floor(Math.random()*statuses.length)];
-
-      client.user.setActivity(`${status}`, { type: 'WATCHING' })
-  }, 10000)
+client.shard.fetchClientValues('guilds.cache.size')
+    .then(results => {
+        client.user.setActivity(`${results.reduce((prev, guildCount) => prev + guildCount, 0)} total servers!`);
+    })
+    .catch(console.error);
 }
