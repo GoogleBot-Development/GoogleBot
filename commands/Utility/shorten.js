e10bb62f2e0715ee52a62f1e522f456d46908a4d
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-03 13:25:19 -0500

    Update shorten.js

--- commands/Utility/shorten.js
+++ commands/Utility/shorten.js
@@ -3,6 +3,7 @@ const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year,
 const TinyURL = require('tinyurl');
 
 exports.run = (client, message, args) => {
+    if(!args[0]) return message.reply("you must provide a link to shorten!")
     var mes = message.content.split(" ").slice(1).join(" ");
     var mes = encodeURI(message.content.split(" ").slice(1).join(" "))
     TinyURL.shorten(mes, function(res) {
