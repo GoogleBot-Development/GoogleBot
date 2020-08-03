40ebb613c1e208cec2a875a994a1ce3e406f7534
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-03 15:21:49 -0500

    Update links.js

--- commands/Utility/links.js
+++ commands/Utility/links.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
 let linksEmbed = new Discord.MessageEmbed()
     .setColor("RANDOM")
     .setAuthor("GoogleBot Links", message.author.displayAvatarURL( {format: "png" }))
-    .setDescription("**[Bot Invite Link](https://discord.com/oauth2/authorize/?permissions=392256&scope=bot&client_id=721215949088358420)**\n\n**[Support Server Invite Link](https://discord.gg/4a9pk8q)**\n\n**[Discord Bots](https://discord.bots.gg/bots/721215949088358420)**\n\n**[Discord Bot List](https://top.gg/bot/721215949088358420)**\n\n**[Chaotic Destiny Hosting Invite](https://discord.gg/qxbhFX8)**")
+    .setDescription("**[Bot Invite Link](https://discord.com/oauth2/authorize/?permissions=392256&scope=bot&client_id=721215949088358420)**\n\n**[Support Server Invite Link](https://discord.gg/4a9pk8q)**\n\n**[Discord Bots](https://discord.bots.gg/bots/721215949088358420)**\n\n**[Discord Bot List](https://top.gg/bot/721215949088358420)**")
     .setTimestamp()
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( {format: "png"} ))
     message.channel.send(linksEmbed)
