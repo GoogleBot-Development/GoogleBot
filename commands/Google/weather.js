3eed2d95601ff42652b8551224d03df947dae573
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-03 15:19:48 -0500

    Update weather.js

--- commands/Google/weather.js
+++ commands/Google/weather.js
@@ -38,7 +38,7 @@ exports.run = (client, message, args) => {
 exports.help = {
   name: "weather",
   description: "I can display the weather! Just give me a city or a town that I can scan.",
-  usage: "weather <city/town>",
+  usage: "weather [city/town]",
   category: "Google"
 }
 
