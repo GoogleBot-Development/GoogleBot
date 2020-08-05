1db303db3db09ad52dacd320111c42c73a501d8e
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-05 18:37:23 -0500

    Update help.js

--- commands/Important/help.js
+++ commands/Important/help.js
@@ -4,7 +4,7 @@ const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year,
 
 exports.run = (client, message, args) => {
  args = args.join(" ")
-   let fun = [], actions = [], google = [], important = [], links = [], utility = []
+   let fun = [], actions = [], google = [], important = [], utility = [], links = []
 
   if (!args) {
    
@@ -19,7 +19,7 @@ recursive("./commands/", function (err, files) {
     if (f.startsWith("commands/Google")) google.push(`\`${props.help.name}\``)
     if (f.startsWith("commands/Actions")) actions.push(`\`${props.help.name}\``)
     if (f.startsWith("commands/Important")) important.push(`\`${props.help.name}\``)
-   if (f.startsWith("commands/Links")) links.push(`\`${props.help.name}\``)
+    if (f.startsWith("commands/Links")) links.push(`\`${props.help.name}\``)
 })
 
   const embed = new Discord.MessageEmbed()
