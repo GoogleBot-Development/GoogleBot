c1dececcf1aaa115193b92e84cec0277d62c9819
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-07 19:04:05 -0500

    Update set.js

--- commands/Admin/set.js
+++ commands/Admin/set.js
@@ -10,13 +10,18 @@ if (!giveIDs.includes(message.author.id)) return
 
   let user = message.mentions.members.first() || message.author
 
+  if (args[1] === "wallet") currency = "Wallet"
+  if (args[1] === "bank") currency = "Bank"
+  if (args[1] === "fish") currency = "Fish"
+  if (args[1] === "wheat") currency = "Wheat"
+
     if (isNaN(args[2])) return;
     economy.set(`${args[1]}_${user.id}`, args[2])
     let bal = await economy.get(`${args[1]}_${user.id}`)
 
     let moneyEmbed = new Discord.MessageEmbed()
     .setColor("RED")
-    .setDescription(`<a:checkmark:736406591275794583> Set ${user}'s ${args[1]} to **${args[2]}** GoogleCoins`)
+    .setDescription(`<a:checkmark:736406591275794583> Set ${user}'s ${currency} to **${args[2]}** GoogleCoins`)
     .setTimestamp()
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
     message.channel.send(moneyEmbed)
