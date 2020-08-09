b7c26691f6b69fc8f8780ba69ff08eb5fb3b3c18
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2020-08-08 21:54:44 -0500

    Create webhook.js

---
+++ webhook.js
@@ -0,0 +1,28 @@
+const secret = "GoogleBot123";
+const http = require('http');
+const crypto = require('crypto')
+const {exec} = require('child_process')
+
+
+http.createServer((req, res) => {
+    req.on('data', async chunk => {
+      const signature = `sha1=${crypto.createHmac('sha1', secret).update(chunk).digest('hex')}`;
+ 
+      const isAllowed = req.headers['x-hub-signature'] === signature;
+ 
+      const body = JSON.parse(chunk);
+ 
+      const isMaster = body?.ref === 'refs/heads/master';
+ 
+      if (isAllowed && isMaster) {
+        try {
+        await exec('git pull')
+        await exec('pm2 restart index')
+        } catch (err) {
+        console.log(err)
+         }
+      }
+    });
+ 
+    res.end();
+  }).listen(8080);
