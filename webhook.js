1d115d13e9e35d29aa95e9d4eb0cff2907eb1007
Ubuntu <Jeydin21@GoogleBot.g4h4ojt0lssepkln4mwbbhknua.bx.internal.cloudapp.net>
2020-08-09 02:50:39 +0000

    xd

--- webhook.js
+++ webhook.js
@@ -1,20 +1,28 @@
 const secret = "GoogleBot123";
 const http = require('http');
-const createHandler = require('github-webhook-handler')
-const handler = createHandler({ path: '/webhook', secret: secret });
+const crypto = require('crypto')
+const {exec} = require('child_process')
 
 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(8080)
-
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-});
+http.createServer((req, res) => {
+    req.on('data', async chunk => {
+      const signature = `sha1=${crypto.createHmac('sha1', secret).update(chunk).digest('hex')}`;
  
-handler.on('push', function (event) {
-  console.log("it worked!")
-});
+      const isAllowed = req.headers['x-hub-signature'] === signature;
+ 
+      const body = JSON.parse(chunk);
+ 
+      const isMaster = body?.ref === 'refs/heads/master';
+ 
+      if (isAllowed && isMaster) {
+        try {
+        await exec('git pull')
+        await exec('pm2 restart index')
+        } catch (err) {
+        console.log(err)
+         }
+      }
+    });
+ 
+    res.end();
+  }).listen(8080);
