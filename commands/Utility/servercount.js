3c139fd45f8c0b5789763e7bda8ce81d2a9f0877
ThatMajesticGuy <31870093+ThatMajesticGuy@users.noreply.github.com>
2020-08-13 19:50:01 -0500

    Update servercount.js

--- commands/Utility/servercount.js
+++ commands/Utility/servercount.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js')
 const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
 
-exports.run = (client, message, args) => {
-client.shard.fetchClientValues('guilds.cache.size')
-    .then(result => {
-      message.channel.send(`Total Shards: 3\nTotal Servers per Shard:\n${result}`)
-    })
-    client.shard.fetchClientValues('guilds.cache.size')
-	.then(results => {
-		message.channel.send(`${results.reduce((prev, guildCount) => prev + guildCount, 0)} total servers!`);
-	})
-	.catch(console.error);
+exports.run = async (client, message, args) => {
+  const results = await client.shard.fetchClientValues('guilds.cache.size')
+
+  let shardCount = []
+  results.forEach((result, r) => shardCount.push(`**Shard ${r + 1}**: ${result.toString()} Servers`))
+  const embed = new Discord.MessageEmbed()
+  .setColor("RANDOM")
+  .setAuthor("GoogleBot's Severcount", message.author.displayAvatarURL())
+  .setDescription(`**Total Shards**: ${results.length}\n\n${shardCount.join("\n")}\n\n**${results.reduce((prev, guildCount) => prev + guildCount, 0).toString()}** total servers!`)
+  message.channel.send(embed)
 }
 
 exports.help = {
