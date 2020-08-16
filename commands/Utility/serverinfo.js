995f6e1a57d63d120a58028cc68ac88a335ec0aa
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-15 20:25:01 -0500

    Update serverinfo.js

--- commands/Utility/serverinfo.js
+++ commands/Utility/serverinfo.js
@@ -5,8 +5,19 @@ const dateformat = require('dateformat')
 
 exports.run = (client, message, args, guild) => {
   let roles = message.guild.roles.cache.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(role => `<@&${role.id}>`); 
+  let pie = message.guild.roles.cache.size
 if (roles.length === 0) roles = ["No Roles"]
 if (roles.length > 30) roles = ["This server has too many roles for me to display!"]
+
+let emojis = message.guild.emojis.cache.array()
+let chicken = message.guild.emojis.cache.size
+if (emojis.length === 0) emojis = ["No Emojis"], chicken = ["0"]
+if (emojis.length > 70) emojis = ["This server has too many emojis for me to display!"]
+
+let channels = message.guild.channels.cache.size - message.guild.channels.cache.filter(channel => channel.type == "category").size
+
+let bots = message.guild.members.cache.filter(member => member.user.bot).size
+let users = message.guild.memberCount - bots
   let serverinfoEmbed = new Discord.MessageEmbed()
     .setColor("RANDOM")
     .setTitle("Server Info")
@@ -18,10 +29,10 @@ if (roles.length > 30) roles = ["This server has too many roles for me to displa
     .addField("Server ID", message.guild.id, true)
     .addField("Server Created", require('dateformat')(message.guild.createdAt, "mmmm d, yyyy"), true)
     .addField("\u200b", "\u200b", true)
-    .addField("Member Count", message.guild.memberCount, true)
-    .addField("Channel Count", message.guild.channels.cache.size, true)
-    .addField("Role Count", message.guild.roles.cache.size, true)
-    .addField("Roles", roles.join(", "))
+    .addField(`Member Count - (${message.guild.memberCount})`, `${message.guild.members.cache.filter(member => member.user.bot).size} bots, ${users} users`, true)
+    .addField(`Channel Count - (${channels})`, `${message.guild.channels.cache.filter(channel => channel.type == "text").size} text, ${message.guild.channels.cache.filter(channel => channel.type == "voice").size} voice, ${message.guild.channels.cache.filter(channel => channel.type == "news").size} annoucnement, ${message.guild.channels.cache.filter(channel => channel.type == "store").size} store`, true)
+    .addField(`Roles - (${pie})`, roles.join(", "))
+    .addField(`Emojis - (${chicken})`, emojis.join(", "))
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
     .setTimestamp()
     message.channel.send(serverinfoEmbed)
