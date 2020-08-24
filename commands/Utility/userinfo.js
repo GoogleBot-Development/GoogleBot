3a50593aa278e9c57abddbf1094195262147db38
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-24 18:25:46 -0500

    Update userinfo.js

--- commands/Utility/userinfo.js
+++ commands/Utility/userinfo.js
@@ -9,13 +9,13 @@ const status = {
    dnd: '<a:dnd:738279357382131715> Do Not Disturb',
    offline: '<a:offline:738279367787937835> Offline/Invisible'
  };
- let user = message.mentions.users.first() || message.author
+ let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author
  let member = message.guild.members.cache.get(user.id)
 
 let presence = member.presence.activities[0] || {name: "Not playing a game"}
 
 
-let roles = member.roles.cache.filter(x => x.name !== '@everyone').sort((a,b) =>{return b.position - a.position}).map(role => `<@&${role.id}>`).reverse() 
+let roles = member.roles.cache.filter(x => x.name !== '@everyone').sort((a,b) =>{return b.position - a.position}).map(role => `<@&${role.id}>`); 
 let pie = roles.length
 if (roles.length === 0) roles = ["No Roles"]
 if (roles.length > 20) roles = ["You have too many roles for me to display!"]
@@ -24,13 +24,13 @@ let embed = new Discord.MessageEmbed()
 .setAuthor(`${user.username}'s Info`, message.author.displayAvatarURL({dynamic: true}))
 .setThumbnail(user.displayAvatarURL({dynamic: true}))
 .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
-.addField("Username", user.tag, true)
-.addField("ID", user.id, true)
-.addField("Status", status[member.presence.status], true)
-.addField("Playing", `${presence.name}` || "Not playing a game", true)
-.addField("Account Created", require('dateformat')(user.createdAt, "mmmm d, yyyy"), true)
-.addField(`Joined ${message.guild.name} on`, require('dateformat')(member.joinedAt, "mmmm d, yyyy"), true)
-.addField(`Roles - (${pie})`, roles.join(", "))
+.addField(":bust_in_silhouette: Username", user.tag, true)
+.addField(":id: ID", user.id, true)
+.addField(":label: Status", status[member.presence.status], true)
+.addField(":video_game: Playing", `${presence.name}` || "Not playing a game", true)
+.addField(":calendar_spiral: Account Created", require('dateformat')(user.createdAt, "mmmm d, yyyy"), true)
+.addField(`:calendar: Joined ${message.guild.name} on`, require('dateformat')(member.joinedAt, "mmmm d, yyyy"), true)
+.addField(`:page_facing_up: Roles - (${pie})`, roles.join(", "))
 message.channel.send(embed)
 }
 
