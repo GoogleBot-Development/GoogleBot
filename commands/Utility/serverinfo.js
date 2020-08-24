9fdac27543faf6a2b41cb125f3ed1a0c4a86534c
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-24 18:24:53 -0500

    Update serverinfo.js

--- commands/Utility/serverinfo.js
+++ commands/Utility/serverinfo.js
@@ -12,7 +12,7 @@ if (roles.length > 30) roles = ["This server has too many roles for me to displa
 let emojis = message.guild.emojis.cache.array()
 let chicken = message.guild.emojis.cache.size
 if (emojis.length === 0) emojis = ["No Emojis"], chicken = ["0"]
-if (emojis.length > 70) emojis = ["This server has too many emojis for me to display!"]
+if (emojis.length > 20) emojis = ["This server has too many emojis for me to display!"]
 
 let channels = message.guild.channels.cache.size - message.guild.channels.cache.filter(channel => channel.type == "category").size
 
@@ -20,19 +20,18 @@ let bots = message.guild.members.cache.filter(member => member.user.bot).size
 let users = message.guild.memberCount - bots
   let serverinfoEmbed = new Discord.MessageEmbed()
     .setColor("RANDOM")
-    .setTitle("Server Info")
-    .setThumbnail(message.guild.iconURL( { format: "png" } ))
-    .setAuthor(`${message.guild.name}'s Info`, message.author.displayAvatarURL( { format: "png" } ))
-    .addField("Server Name", message.guild.name, true)
-    .addField("Server Owner", `<@!${message.guild.owner.id}>`, true)
+    .setThumbnail(message.guild.iconURL( { dynamic: true} ))
+    .setAuthor(`${message.guild.name}'s Info`, message.author.displayAvatarURL( {dynamic: true} ))
+    .addField(":card_index: Server Name", message.guild.name, true)
+    .addField(":crown: Server Owner", `<@!${message.guild.owner.id}>`, true)
     .addField("\u200b", "\u200b", true)
-    .addField("Server ID", message.guild.id, true)
-    .addField("Server Created", require('dateformat')(message.guild.createdAt, "mmmm d, yyyy"), true)
+    .addField(":id: Server ID", message.guild.id, true)
+    .addField(":calendar_spiral: Server Created", require('dateformat')(message.guild.createdAt, "mmmm d, yyyy"), true)
     .addField("\u200b", "\u200b", true)
-    .addField(`Member Count - (${message.guild.memberCount})`, `${message.guild.members.cache.filter(member => member.user.bot).size} bots, ${users} users`, true)
-    .addField(`Channel Count - (${channels})`, `${message.guild.channels.cache.filter(channel => channel.type == "text").size} text, ${message.guild.channels.cache.filter(channel => channel.type == "voice").size} voice, ${message.guild.channels.cache.filter(channel => channel.type == "news").size} annoucnement, ${message.guild.channels.cache.filter(channel => channel.type == "store").size} store`, true)
-    .addField(`Roles - (${pie})`, roles.join(", "))
-    .addField(`Emojis - (${chicken})`, emojis.join(", "))
+    .addField(`:busts_in_silhouette: Member Count - (${message.guild.memberCount})`, `${message.guild.members.cache.filter(member => member.user.bot).size} bots, ${users} users`, true)
+    .addField(`:scroll: Channel Count - (${channels})`, `${message.guild.channels.cache.filter(channel => channel.type == "text").size} text, ${message.guild.channels.cache.filter(channel => channel.type == "voice").size} voice, ${message.guild.channels.cache.filter(channel => channel.type == "news").size} annoucnement, ${message.guild.channels.cache.filter(channel => channel.type == "store").size} store`, true)
+    .addField(`:page_facing_up: Roles - (${pie})`, roles.join(", "))
+    .addField(`:smile: Emojis - (${chicken})`, emojis.join(", "))
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
     .setTimestamp()
     message.channel.send(serverinfoEmbed)
