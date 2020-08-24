41f4b368d929b2c74371dae743c03d0495feda8f
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-24 17:28:21 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose')
+const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../config.json");
 
 module.exports = (client) => {
   console.log(`The shard launched successfully!`)
   mongoose.connect(require('../config.json').mongo, {useNewUrlParser: true, useUnifiedTopology: true})
-  
-  setInterval(function() {
-      let statuses = [ `g!help | 3 shards`, `people ping me...`, `g!help | Over 2500 servers!`, `Discord verify bots...` ];
-      let status = statuses[Math.floor(Math.random()*statuses.length)];
 
-      client.user.setActivity(`${status}`, { type: 'WATCHING' })
-  }, 10000)
+    setInterval(function() {
+        let links = [`https://www.twitch.tv/Jeydin21`, "https://www.twitch.tv/Jeydin21"];
+        let statusLinks = links[Math.floor(Math.random()*links.length)];
+
+        let statuses = ["g!help | g!invite", `g!help | ${version}`, "to your server!"]
+        let status = statuses[Math.floor(Math.random()*statuses.length)];
+  
+        client.user.setActivity(status, {
+            type: "STREAMING",
+            url: statusLinks
+          });
+    }, 10000)
 }
