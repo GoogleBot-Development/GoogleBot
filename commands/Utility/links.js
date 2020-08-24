9046e02a85a8c38370ae9c5fa14a9d698881dc9c
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-08-24 18:21:58 -0500

    Update links.js

--- commands/Utility/links.js
+++ commands/Utility/links.js
@@ -5,7 +5,7 @@ const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year,
 exports.run = async (client, message, args) => {
 let linksEmbed = new Discord.MessageEmbed()
     .setColor("RANDOM")
-    .setAuthor("GoogleBot Links", message.author.displayAvatarURL( {format: "png" }))
+    .setAuthor("GoogleBot Links", message.author.displayAvatarURL({dynamic: true}))
     .setDescription("**[Bot Invite Link](https://discord.com/oauth2/authorize/?permissions=392256&scope=bot&client_id=721215949088358420)**\n\n**[Support Server Invite Link](https://discord.gg/4a9pk8q)**\n\n**[Discord Bots](https://discord.bots.gg/bots/721215949088358420)**\n\n**[Discord Bot List](https://top.gg/bot/721215949088358420)**")
     .setTimestamp()
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( {format: "png"} ))
