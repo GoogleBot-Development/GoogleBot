2a6a2a90976777b1fd053c387066b00d8815837f
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-09-18 22:37:20 -0500

    Create start.js

---
+++ functions/start.js
@@ -0,0 +1,15 @@
+// Initialize the User
+
+
+module.exports = (user) => {
+    new require('../models/user.js')({
+      id: user.id,
+      username: `${user.tag}`,
+      money: {wallet: 50, bank: 0},
+      times: {beg: null, daily: null, fishing: null, wheat: null, weekly: null, work: null},
+      fish: 5,
+      wheat: 20
+    }).save()
+    console.log(`Created a profile for ${user.username}#${user.discriminator}`)
+    return {id: user.id, username: `${user.tag}`, money: {wallet: 0, bank: 0}, times: {beg: null, daily: null, fishing: null, wheat: null, weekly: null, work: null}, fish: 5, wheat: 20}
+  }
