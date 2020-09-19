dfcd370c4ead14012dca9b3d340fe0e2895da6fa
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-09-19 09:03:24 -0500

    Update serverinfo.js

--- commands/Utility/serverinfo.js
+++ commands/Utility/serverinfo.js
@@ -6,6 +6,7 @@ const dateformat = require('dateformat')
 exports.run = (client, message, args, guild) => {
   let roles = message.guild.roles.cache.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(role => `<@&${role.id}>`); 
   let pie = message.guild.roles.cache.size
+  let ahaha = [`This server is on Shard ${message.guild.shard.id + 1}!`]
 if (roles.length === 0) roles = ["No Roles"]
 if (roles.length > 30) roles = ["This server has too many roles for me to display!"]
 
@@ -30,6 +31,7 @@ let users = message.guild.memberCount - bots
     .addField("\u200b", "\u200b", true)
     .addField(`:busts_in_silhouette: Member Count - (${message.guild.memberCount})`, `${message.guild.members.cache.filter(member => member.user.bot).size} bots, ${users} users`, true)
     .addField(`:scroll: Channel Count - (${channels})`, `${message.guild.channels.cache.filter(channel => channel.type == "text").size} text, ${message.guild.channels.cache.filter(channel => channel.type == "voice").size} voice, ${message.guild.channels.cache.filter(channel => channel.type == "news").size} annoucnement, ${message.guild.channels.cache.filter(channel => channel.type == "store").size} store`, true)
+    .addField(`:ice_cube: Shard ID`, ahaha)
     .addField(`:page_facing_up: Roles - (${pie})`, roles.join(", "))
     .addField(`:smile: Emojis - (${chicken})`, emojis.join(", "))
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
