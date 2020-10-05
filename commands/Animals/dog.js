4d1aa49e5380f77284228f67486684827cdf8d1c
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-10-05 13:53:19 -0500

    Update dog.js

--- commands/Animals/dog.js
+++ commands/Animals/dog.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js");
 const randomPuppy = require("random-puppy");
 
 exports.run = async (client, message, args) => {
+    let msg = await message.channel.send('Generating image...')
 
     const subReddits = ["dogpictures"];
         
@@ -14,7 +15,7 @@ exports.run = async (client, message, args) => {
             .setImage(img)
             .setFooter(`Random dog from r/${random}`)
 
-        message.channel.send(embed);
+        msg.edit(" ", embed);
 }
 
 exports.help = {
