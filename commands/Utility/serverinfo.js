a2da7b0a594ee939d8615fbf0ae62495b0331cae
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-10-05 14:15:07 -0500

    Update serverinfo.js

--- commands/Utility/serverinfo.js
+++ commands/Utility/serverinfo.js
@@ -14,6 +14,22 @@ let emojis = message.guild.emojis.cache.array()
 let chicken = message.guild.emojis.cache.size
 if (emojis.length === 0) emojis = ["No Emojis"], chicken = ["0"]
 if (emojis.length > 20) emojis = ["This server has too many emojis for me to display!"]
+  
+  let region = {
+    "brazil": ":flag_br: Brazil",
+    "europe": ":flag_eu: Europe",
+    "singapore": ":flag_sg: Singapore",
+    "us-central": ":flag_us U.S. Central",
+    "southafrica": ":flag_za: South Africa",
+    "sydney": ":flag_au: Sydney",
+    "us-east": ":flag_us: U.S. East",
+    "us-south": ":flag_us: U.S. South",
+    "us-west": ":flag_us: U.S. West",
+    "russia": ":flag_ru: Russia",
+    "india": ":flag_in: India",
+    "japan": ":flag_jp: Japan",
+    "hong-kong": ":flag_hk: Hong Kong"
+  }
 
 let channels = message.guild.channels.cache.size - message.guild.channels.cache.filter(channel => channel.type == "category").size
 
@@ -32,6 +48,7 @@ let users = message.guild.memberCount - bots
     .addField(`:busts_in_silhouette: Member Count - (${message.guild.memberCount})`, `${message.guild.members.cache.filter(member => member.user.bot).size} bots, ${users} users`, true)
     .addField(`:scroll: Channel Count - (${channels})`, `${message.guild.channels.cache.filter(channel => channel.type == "text").size} text, ${message.guild.channels.cache.filter(channel => channel.type == "voice").size} voice, ${message.guild.channels.cache.filter(channel => channel.type == "news").size} annoucnement, ${message.guild.channels.cache.filter(channel => channel.type == "store").size} store`, true)
     .addField(`:ice_cube: Shard ID`, ahaha)
+    .addField(`:flag_white: Server Region`, region[message.guild.region])
     .addField(`:page_facing_up: Roles - (${pie})`, roles.join(", "))
     .addField(`:smile: Emojis - (${chicken})`, emojis.join(", "))
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
