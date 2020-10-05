b893159ad3033e41041c6d79f33a48246a74a91b
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-10-05 18:16:35 -0500

    Update start.js

--- functions/start.js
+++ functions/start.js
@@ -10,6 +10,6 @@ module.exports = (user) => {
       fish: 5,
       wheat: 20
     }).save()
-    console.log(`Created a profile for ${user.username}#${user.discriminator}`)
+    console.log(`A new profile was just created for ${user.username}#${user.discriminator}!`)
     return {id: user.id, username: `${user.tag}`, money: {wallet: 0, bank: 0}, times: {beg: null, daily: null, fishing: null, wheat: null, weekly: null, work: null}, fish: 5, wheat: 20}
   }
