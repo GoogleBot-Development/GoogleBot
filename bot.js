5e6847a2f0db4106faec804263e2d6548997a7b7
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-10-11 22:25:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.aliases = new Discord.Collection()
 client.on("ready", async () => {
   client.shard.fetchClientValues('guilds.cache.size')
 	.then(results => {
-		let food = (`${results.reduce((prev, guildCount) => prev + guildCount, 0)}`);
+		const food = (`${results.reduce((prev, guildCount) => prev + guildCount, 0)}`);
 setInterval(() => {
   snekfetch.post(`https://discordbots.org/api/bots/stats`)
     .set('Authorization', `${dblToken}`)
@@ -131,6 +131,7 @@ client.on('guildCreate', message => {
     let webhook = new Discord.WebhookClient("739962361548505239", "Bhe017bH-H_7TcgBGkYvJTl__0ENLYdrk2UldwJax8XMC0dp4wVs0neKlGLqlmv5WZM6")
     
     webhook.send(joinEmbed)
+    console.log(`[Shard #${shard}] I just joined ${guild.name} (${guild.id}) which is owned by ${guild.owner.tag} (${guild.owner.id}). I am now in ${food} servers!`)
 	  
 	let defaultChannel = "";
 guild.channels.cache.forEach((channel) => {
@@ -171,6 +172,7 @@ client.on('guildDelete', message => {
     let webhook = new Discord.WebhookClient("739965365248852029", "MUVBrXRpxfb9mhohMsUmKcWLTodwghqnSEqPNxnvsJwzA5qFEQyGhju3oEQ1NYfnXDVe")
     
     webhook.send(leaveEmbed)
+    console.log(`[Shard #${shard}] I just left ${guild.name} (${guild.id}) which was owned by ${guild.owner.tag} (${guild.owner.id}). I am now in ${food} servers!`)
   }).catch(console.error)
 })
  
