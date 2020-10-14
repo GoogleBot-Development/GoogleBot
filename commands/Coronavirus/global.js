9f34ee0fb4e9d96aefe3053b962279fa1cd63e1c
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-10-13 22:36:52 -0500

    Create global.js

---
+++ commands/Coronavirus/global.js
@@ -0,0 +1,41 @@
+/* eslint-disable quotes */
+const Discord = require('discord.js');
+const covid = require('covidtracker');
+const fetch = require('node-fetch');
+
+Object.defineProperty(String.prototype, 'toProperCase', {
+  value: function() {
+    return this.replace(/([^\W_]+[^\s-]*) */g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+  },
+});
+
+exports.run = async (client, message, args) => {
+      const totalStats = await covid.getAll();
+
+      const updatedTime = new Date(totalStats.updated);
+
+      const embed = new Discord.MessageEmbed()
+        .setAuthor('Global Coronavirus Statistics', message.author.displayAvatarURL({dynamic: true}))
+        .addField('Confirmed Cases', `**${totalStats.cases.toLocaleString()}**`, true)
+        .addField('Today\'s Cases', `+${totalStats.todayCases.toLocaleString()}`, true)
+        .addField('Today\'s Deaths', `+${totalStats.todayDeaths.toLocaleString()}`, true)
+        .addField('Active Cases', `${totalStats.active.toLocaleString()} (${((totalStats.active / totalStats.cases) * 100).toFixed(2)}%)`, true)
+        .addField('Recovered Cases', `${totalStats.recovered.toLocaleString()} (${((totalStats.recovered / totalStats.cases) * 100).toFixed(2)}%)`, true)
+        .addField('Deaths', `${totalStats.deaths.toLocaleString()} (${((totalStats.deaths / totalStats.cases) * 100).toFixed(2)}%)`, true)
+        .addField('Tests', `${totalStats.tests.toLocaleString()}`, true)
+        .addField('Cases Per Million', `${totalStats.casesPerOneMillion.toLocaleString()}`, true)
+        .addField('Deaths Per Million', `${totalStats.deathsPerOneMillion.toLocaleString()}`, true)
+        .setImage(`https://xtrading.io/static/layouts/qK98Z47ptC-embed.png?newest=${Date.now()}`)
+        .setColor("RANDOM")
+        .setFooter(`Last Updated: ${updatedTime}`);
+      message.channel.send(embed);
+    }
+
+exports.help = {
+  name: "global",
+  description: "Gives the global coronavirus statistics",
+  usage: "global",
+  category: "Coronavirus"
+}
+
+exports.aliases = []
