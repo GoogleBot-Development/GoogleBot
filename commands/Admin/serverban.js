42c587ae49cd2fdee27725fc24d69b12155dbbf0
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-10-17 21:03:02 -0500

    Update serverban.js

--- commands/Admin/serverban.js
+++ commands/Admin/serverban.js
@@ -3,15 +3,15 @@ var serverban = require("../../serverbans.json")
 
 exports.run = async (client, message, args) => {
   if(message.author.id !== "667354950321569792") return
-  			let server = client.shard.broadcastEval(`bot.guilds.cache.get(args[0])`)
+	let server = args[0]
         let reason = args.slice(1).join(" ")
         if(!server) return message.channel.send("You must put a server to add to the Server Bot Ban Database!")
         if(!reason) return message.channel.send("You must give a reason!")
-				serverban[server.id] = {'servername': server.name, 'reason': reason}
+				serverban[server] = {'reason': reason}
 				fs.writeFile("./serverbans.json", JSON.stringify(serverban, null, 4), function(err){
 	          	if (err) { console.log(log_time() + log_error + err) }
 	            })
-        message.channel.send(`Added the server, **${server.name}** (**${server.id}**), to the Server Bot Ban Database! Reason: **${reason}**`)
+        message.channel.send(`Added the server to the Server Bot Ban Database! Reason: ${reason}`)
 };
 
 exports.help = {
