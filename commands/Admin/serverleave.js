9a2cafa5caa6f8245f80078e999e4d6e77e2a364
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-11-01 16:21:01 -0600

    Update serverleave.js

--- commands/Admin/serverleave.js
+++ commands/Admin/serverleave.js
@@ -3,22 +3,21 @@ const client = new Discord.Client()
 
 exports.run = async (client, message, args) => {
 if(message.author.id !== "667354950321569792") return
-  let id = args[0]
-  if(!id) return message.channel.send("You must give a server ID to make me leave!")
+ if(!args[0]) return message.channel.send("You must give a server ID to make me leave!")
   
   let fish = message.guild.shard.id + 1
   
-  let server = message.client.shard.broadcastEval("this.guilds.cache.get(id)")
+  let server = message.client.shard.broadcastEval("this.guilds.cache.get(args[0])")
   
   let reason = args.slice(1).join(" ")
   
   if(!reason) return message.channel.send("You must give me a reason to make me leave the server!")
   
-  message.client.shard.broadcastEval("this.guilds.cache.get(id).leave()")
+  message.client.shard.broadcastEval("this.guilds.cache.get(args[0]).leave()")
   
-  message.channel.send(`I have just left: ${server.name} (${id}) with reason: ${reason}`)
+  message.channel.send(`I have just left: ${server.name} (${args[0]}) with reason: ${reason}`)
   
-  console.log(`[Shard #${fish}] I was forced to leave: ${server.name} (${id}) with reason: ${reason}`)
+  console.log(`[Shard #${fish}] I was forced to leave: ${server.name} (${args[0]}) with reason: ${reason}`)
 }
 
 exports.help = {
