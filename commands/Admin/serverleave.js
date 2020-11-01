67c3f550e3979c52e90da25a1b3b0782f0fec309
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-11-01 11:24:30 -0600

    Update serverleave.js

--- commands/Admin/serverleave.js
+++ commands/Admin/serverleave.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
 if(message.author.id !== "667354950321569792") return
   let fish = message.guild.shard.id + 1
   let serverid = args[0]
-  let reason = args.join(" ").slice(2)
+  let reason = args.slice(1).join(" ")
   if(!serverid) return message.channel.send("You must give a server ID to make me leave!")
   if(!reason) return message.channel.send("You must give me a reason to make me leave the server!")
   let server = client.shard.broadcastEval("this.guilds.cache.get(`${serverid}`)")
