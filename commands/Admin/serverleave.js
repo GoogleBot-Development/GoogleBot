e48599bfab55b4a94c18ab7d3633779da2cf3a56
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-11-01 11:39:03 -0600

    Update serverleave.js

--- commands/Admin/serverleave.js
+++ commands/Admin/serverleave.js
@@ -4,12 +4,11 @@ const client = new Discord.Client()
 exports.run = async (client, message, args) => {
 if(message.author.id !== "667354950321569792") return
   let fish = message.guild.shard.id + 1
-  let serverid = args[0]
+  let server = client.shard.broadcastEval(this.guilds.cache.get(args[0]))
   let reason = args.slice(1).join(" ")
-  if(!serverid) return message.channel.send("You must give a server ID to make me leave!")
+  if(!server) return message.channel.send("You must give a server ID to make me leave!")
   if(!reason) return message.channel.send("You must give me a reason to make me leave the server!")
-  let server = client.shard.broadcastEval("this.guilds.cache.get(`${serverid}`)")
-  client.shard.broadcastEval("this.guilds.cache.get(`${serverid}`).leave()")
+  client.shard.broadcastEval(this.guilds.cache.get(server.id).leave())
   message.channel.send(`I have just left: ${server.name} (${server.id}) with reason: ${reason}`)
   console.log(`[Shard #${fish}] I was forced to leave: ${server.name} ({server.id}) with reason: ${reason}`)
 }
