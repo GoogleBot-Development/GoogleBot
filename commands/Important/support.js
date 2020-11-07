58b79c490d48f1e9a5cf9d25ca21efa71e0d4b01
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-11-07 17:05:56 -0600

    Update support.js

--- commands/Important/support.js
+++ commands/Important/support.js
@@ -5,7 +5,9 @@ const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year,
 exports.run = (client, message, args) => {
   let supportEmbed = new Discord.MessageEmbed()
     .setColor("RANDOM")
-    .setDescription("Click [here](https://discord.gg/y6Zxauk) to join the support server! We do Discord Nitro Classic giveaways at every 1000 servers, so join to be included! You can get my server count using `g!servercount`!")
+    .setTitle("Click Here To Join The Support Server")
+    .setURL("https://discord.gg/y6Zxauk")
+    .setDescription("Click on the link above to join the support server for GoogleBot! We do Discord Nitro Classic giveaways at every 1000 servers, so join to be included! You can get my server count using `g!servercount`!")
     .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( {format: "png"} ))
     .setTimestamp()
     message.channel.send(supportEmbed)
