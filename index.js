43d78b8eba2a6c997d2a09d62ef52cf7de6a171b
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-11-14 12:51:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const { ShardingManager } = require('discord.js');
 const config = require('./config.json')
 const manager = new ShardingManager('./bot.js', { token: config.token });
 
-manager.spawn(5)
+manager.spawn(6)
 manager.on('shardCreate', shard => console.log(`Launching Shard ${shard.id+1}!`));
