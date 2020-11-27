c00326b48f59684d3a4f136d4ef6fa45a35ed1ce
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-11-26 21:05:20 -0600

    Update skip.js

--- commands/Music/skip.js
+++ commands/Music/skip.js
@@ -1,13 +1,19 @@
+const Discord = require('discord.js')
+const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
+
 exports.run = async (client, message, args) => {
-    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
-    if (!client.distube.isPlaying(message)) return message.channel.send(`${client.emotes.error} | There is nothing playing!`)
-    let queue = client.distube.skip(message);
-    message.channel.send(`${client.emotes.success} | Skipped! Now playing:\n${queue.songs[0].name}`)
-  }
+  let referEmbed = new Discord.MessageEmbed()
+  .setColor("RED")
+  .setAuthor("Music Has Been Moved to GoogleMusic", message.author.displayAvatarURL({dynamic: true}), "https://top.gg/bot/778028895600377876")
+  .setDescription("Hello! We are sad to inform you that the music commands have been moved to our new project, GoogleMusic, which is capable of search YouTube to play music into your server! We have commands that are usually premium-restricted on other bots that are free for everyone to use! You can view GoogleMusic's top.gg's page by clicking on the link above.")
+  .setTimestamp()
+  .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
+  message.channel.send(referEmbed)
+}
 
   exports.help = {
   name: "skip",
-  description: "Skips the current song.",
+  description: "Music has been moved",
   usage: "skip",
   category: "Music"
 }
