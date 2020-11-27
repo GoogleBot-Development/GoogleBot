77e77d701c05127a2b323688cdd282cd8b7d1964
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-11-26 20:55:06 -0600

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,17 +1,17 @@
+const Discord = require('discord.js')
+
 exports.run = async (client, message, args) => {
-  if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
-  let string = args.join(" ")
-  if (!string) return message.channel.send(`${client.emotes.error} | Please enter a song url or query to search.`)
-  try {
-    client.distube.play(message, string)
-  } catch (e) {
-    message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
-  }
+  let referEmbed = new Discord.MessageEmbed()
+  .setColor("RED")
+  .setAuthor("Music Has Been Moved to GoogleMusic", message.author.displayAvatarURL({dynamic: true}), "https://top.gg/bot/778028895600377876")
+  .setDescription("Hello! We are sad to inform you that the music commands have been moved to our new project, GoogleMusic, which is capable of search YouTube to play music into your server! We have commands that are usually premium-restricted on other bots that are free for everyone to use! You can view GoogleMusic's top.gg's page by clicking on the link above.")
+  .setTimestamp()
+  message.channel.send(referEmbed)
 }
 
 exports.help = {
   name: "play",
-  description: "Plays music in a voice channel!",
+  description: "Music has been moved",
   usage: "play [query]",
   category: "Music"
 }
