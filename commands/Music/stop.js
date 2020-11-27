da36bafa214909d6ec7cd62e7f3baabc49f4d673
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2020-11-26 21:05:43 -0600

    Update stop.js

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -1,13 +1,19 @@
+const Discord = require('discord.js')
+const { prefix, token, version, name, ownerID, ownerUsername, mainVersion, year, bannedIDs, bannedServerIDs, dblToken } = require("../../config.json");
+
 exports.run = async (client, message, args) => {
-    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
-    if (!client.distube.isPlaying(message)) return message.channel.send(`${client.emotes.error} | There is nothing playing!`)
-    client.distube.stop(message);
-    message.channel.send(`${client.emotes.success} | Stopped!`)
-  }
+  let referEmbed = new Discord.MessageEmbed()
+  .setColor("RED")
+  .setAuthor("Music Has Been Moved to GoogleMusic", message.author.displayAvatarURL({dynamic: true}), "https://top.gg/bot/778028895600377876")
+  .setDescription("Hello! We are sad to inform you that the music commands have been moved to our new project, GoogleMusic, which is capable of search YouTube to play music into your server! We have commands that are usually premium-restricted on other bots that are free for everyone to use! You can view GoogleMusic's top.gg's page by clicking on the link above.")
+  .setTimestamp()
+  .setFooter(`© ${name} ${year} | ${version}`, message.client.user.displayAvatarURL( { format: "png" } ))
+  message.channel.send(referEmbed)
+}
 
 exports.help = {
   name: "stop",
-  description: "Makes me stop playing music and makes me leave the channel!",
+  description: "Music has been moved",
   usage: "stop",
   category: "Music"
 }
