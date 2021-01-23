7d24837e1d3beb39e096df175054f0176dcfb060
Jeydin21 <62669147+Jeydin21@users.noreply.github.com>
2021-01-23 17:39:27 -0600

    Update eval.js

--- commands/Admin/eval.js
+++ commands/Admin/eval.js
@@ -1,8 +1,7 @@
 const Discord = require ("discord.js");
-const hastebin = require('hastebin.js');
-const haste = new hastebin({});
+const sourcebin = require('sourcebin_js')
 
-exports.run = async (bot, message, a) => {
+exports.run = async (client, message, a) => {
 
   const clean = text => {
   if (typeof(text) === "string")
@@ -20,7 +19,7 @@ exports.run = async (bot, message, a) => {
     }
 
     if (!args2) {
-      message.channel.send("What do you want me to evaluate hjh?");
+      message.channel.send("What do you want me to evaluate?");
       return;
   }
 
@@ -32,14 +31,20 @@ exports.run = async (bot, message, a) => {
 
 
         if (clean(evaled).length > 1024 || code.length > 1024) {
-          haste.post(clean(evaled)).then(link => {
+          sourcebin.create([{
+            name: `Code by ${message.author.tag}`,
+            content: clean(evaled),
+            languageId: 'js'
+          }]).then(src => {
          var embed = new Discord.MessageEmbed()
          .setTitle("Evaluation Overload!")
          .setColor("#4CEF8B")
          .setDescription("The evaluation was more than 1,024 characters")
-         .addField("Here is the link", `${link}`)
+         .addField("Click on the link below to view the eval result", `${src.url}`)
          message.channel.send({ embed: embed })
-          });
+          }).catch(e => {
+            message.channel.send(`Error, try again later`)
+      });
 } else {
         var embed2 = new Discord.MessageEmbed()
         .setTitle("Evaled:")
@@ -51,14 +56,20 @@ exports.run = async (bot, message, a) => {
     } catch (err) {
         const code = args.join(" ");
                 if (clean(err).length > 1024 || code.length > 1024) {
-        haste.post(clean(err).stack).then(link => {
-          var embed = new Discord.MessageEmbed()
-          .setTitle("Evaluation Overload!")
-          .setColor("#4CEF8B")
-          .setDescription("The evaluation was more than 1,024 characters")
-          .addField("Here is the link", `${link}`)
-          message.channel.send({ embed: embed })
-        });
+                  sourcebin.create([{
+                    name: `Code by ${message.author.tag}`,
+                    content: clean(err),
+                    languageId: 'js'
+                  }]).then(src => {
+                 var embed = new Discord.MessageEmbed()
+                 .setTitle("Evaluation Overload!")
+                 .setColor("#4CEF8B")
+                 .setDescription("The evaluation was more than 1,024 characters")
+                 .addField("Click on the link below to view the eval result", `${src.url}`)
+                 message.channel.send({ embed: embed })
+                  }).catch(e => {
+                    message.channel.send(`Error, try again later`)
+              });
 };
       var embed3 = new Discord.MessageEmbed()
       .setTitle("ERROR:")
